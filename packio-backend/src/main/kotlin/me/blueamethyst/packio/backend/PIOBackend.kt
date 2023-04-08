18b642291da48c0a7f19e44fd3f49867042425f7
DinoMarlir <72864666+DinoMarlir@users.noreply.github.com>
2023-04-08 20:31:16 +0200

    cleanup

--- packio-backend/src/main/kotlin/me/blueamethyst/packio/backend/PIOBackend.kt
+++ packio-backend/src/main/kotlin/me/blueamethyst/packio/backend/PIOBackend.kt
@@ -1,15 +1,9 @@
 package me.blueamethyst.packio.backend
 
-import me.blueamethyst.packio.backend.database.MongoAdapter
-
 object PIOBackend {
-
-    val mongoAdapter = MongoAdapter()
 
     @JvmStatic
     fun main(args: Array<String>) {
         logger.info("starting piobackend...")
-
-        mongoAdapter.startup()
     }
 }
