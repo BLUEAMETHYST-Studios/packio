712bccb1bde22b6dc94746dc63c2b72f2a3aaa98
DinoMarlir <72864666+DinoMarlir@users.noreply.github.com>
2023-04-12 20:26:29 +0200

    cleanup

--- packio-backend/src/main/kotlin/me/blueamethyst/packio/backend/Values.kt
+++ packio-backend/src/main/kotlin/me/blueamethyst/packio/backend/Values.kt
@@ -3,7 +3,6 @@ package me.blueamethyst.packio.backend
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.serialization.json.Json
-import me.bluamethyst.packio.api.PIOApi
 import me.blueamethyst.packio.backend.config.Config
 import me.blueamethyst.packio.database.PIODatabase
 import org.slf4j.LoggerFactory
--- packio-backend/src/main/kotlin/me/blueamethyst/packio/backend/rest/routes/MainRoute.kt
+++ packio-backend/src/main/kotlin/me/blueamethyst/packio/backend/rest/routes/MainRoute.kt
@@ -3,7 +3,6 @@ package me.blueamethyst.packio.backend.rest.routes
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import me.blueamethyst.packio.backend.extensions.respondJson
 
 object MainRoute {
 
